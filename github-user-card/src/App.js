3b3868880bbf16c4320b8156c87460c7b8cdb8fc
Sean Birmingham <seannbirmingham98@gmail.com>
2020-09-01 13:58:45 -0400

    displaying user data

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import User from './components/User';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        'https://cors-anywhere.herokuapp.com/https://api.github.com/users/sean-birmingham'
+      )
+      .then((res) => {
+        console.log('user', res.data);
+        this.setState({ user: res.data });
+      })
+      .catch((err) => console.log('error ', err));
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Github User Card</h1>
+        <User user={this.state.user} />
       </div>
     );
   }
--- github-user-card/src/components/User.js
+++ github-user-card/src/components/User.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const UserCard = () => {
-  return <div></div>;
+const UserCard = (props) => {
+  const {
+    avatar_url,
+    name,
+    login,
+    location,
+    html_url,
+    followers,
+    following,
+    bio
+  } = props.user;
+
+  return (
+    <div>
+      <img src={avatar_url} alt='github avatar' />
+      <h2>{name}</h2>
+      <a href={html_url}>{login}</a>
+      <p>Location: {location}</p>
+      <p>Followers: {followers}</p>
+      <p>Following: {following}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
 };
 
 export default UserCard;
